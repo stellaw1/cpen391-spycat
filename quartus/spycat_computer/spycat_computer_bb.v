8a91ee5d85bdd4d4414c4ef971289922fcd69432
Arnold <arnoldying825@gmail.com>
2022-03-08 19:42:40 -0800

    added quartus files

---
+++ quartus/spycat_computer/spycat_computer_bb.v
@@ -0,0 +1,62 @@
+
+module spycat_computer (
+	system_pll_ref_clk_clk,
+	system_pll_ref_reset_reset,
+	sdram_clk_clk,
+	memory_mem_a,
+	memory_mem_ba,
+	memory_mem_ck,
+	memory_mem_ck_n,
+	memory_mem_cke,
+	memory_mem_cs_n,
+	memory_mem_ras_n,
+	memory_mem_cas_n,
+	memory_mem_we_n,
+	memory_mem_reset_n,
+	memory_mem_dq,
+	memory_mem_dqs,
+	memory_mem_dqs_n,
+	memory_mem_odt,
+	memory_mem_dm,
+	memory_oct_rzqin,
+	hps_io_hps_io_uart0_inst_RX,
+	hps_io_hps_io_uart0_inst_TX,
+	io_acknowledge,
+	io_irq,
+	io_address,
+	io_bus_enable,
+	io_byte_enable,
+	io_rw,
+	io_write_data,
+	io_read_data);	
+
+	input		system_pll_ref_clk_clk;
+	input		system_pll_ref_reset_reset;
+	output		sdram_clk_clk;
+	output	[12:0]	memory_mem_a;
+	output	[2:0]	memory_mem_ba;
+	output		memory_mem_ck;
+	output		memory_mem_ck_n;
+	output		memory_mem_cke;
+	output		memory_mem_cs_n;
+	output		memory_mem_ras_n;
+	output		memory_mem_cas_n;
+	output		memory_mem_we_n;
+	output		memory_mem_reset_n;
+	inout	[7:0]	memory_mem_dq;
+	inout		memory_mem_dqs;
+	inout		memory_mem_dqs_n;
+	output		memory_mem_odt;
+	output		memory_mem_dm;
+	input		memory_oct_rzqin;
+	input		hps_io_hps_io_uart0_inst_RX;
+	output		hps_io_hps_io_uart0_inst_TX;
+	input		io_acknowledge;
+	input		io_irq;
+	output	[15:0]	io_address;
+	output		io_bus_enable;
+	output	[1:0]	io_byte_enable;
+	output		io_rw;
+	output	[15:0]	io_write_data;
+	input	[15:0]	io_read_data;
+endmodule
