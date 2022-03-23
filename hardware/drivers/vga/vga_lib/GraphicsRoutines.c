c049cec5e8ffe62e09d4da3ef8da744780465211
liuyishengalan <liuyishengalan@hotmail.com>
2022-03-22 22:26:01 -0700

    Add function tor detect the touch screen keyboard key

--- hardware/drivers/vga/vga_lib/GraphicsRoutines.c
+++ hardware/drivers/vga/vga_lib/GraphicsRoutines.c
@@ -204,7 +204,7 @@ void GraphicsString(char *string_input, int x, int y, int colour, int background
 	//printf("%s", string_input);
 	int i = 0;
 	while(string_input[i]!='\0') {
-		printf("%c", string_input[i]);
+		//printf("%c", string_input[i]);
 		OutGraphicsCharFont2(x+i*15, y, colour, background, string_input[i], 1);
 		i++;
 	}
@@ -331,18 +331,18 @@ int keyboard_helper (int x, int y, Point p) {
 }
 /*
  (0,239)
- ________________________________
+ ____________________________________________
  |                   8
- | _________50_____________    __
+ | _________50_____________    ______________
  | |                       |  |
  |4|                       | 3|
  | |          ~            |  |
  | 40                      40 |
  | |                       |  |
- | |________50_____________|  |__
+ | |________50_____________|  |_______________
  |                         6   
- |  _______50______________    __
- | |                       |  |
+ |               _______50______________    __
+ |      29      |                       |  |
 */
 char keyboard_response (void) {
 	Point p;
@@ -355,14 +355,303 @@ char keyboard_response (void) {
 		int key_height = 40;
 		int topleft = 239;
 		//1st line
-		if (keyboard_helper(4, 239+8, p)){
+		if (keyboard_helper(4+53*0, 239+8, p)){
 			printf("~");
 			return '~';
 		}
-		if (keyboard_helper(4+53, 239+8, p)){
+		if (keyboard_helper(4+53*1, 239+8, p)){
 			printf("1");
 			return '1';
 		}
+		if (keyboard_helper(4+53*2, 239+8, p)){
+			printf("2");
+			return '2';
+		}
+		if (keyboard_helper(4+53*3, 239+8, p)){
+			printf("3");
+			return '3';
+		}
+		if (keyboard_helper(4+53*4, 239+8, p)){
+			printf("4");
+			return '4';
+		}
+		if (keyboard_helper(4+53*5, 239+8, p)){
+			printf("5");
+			return '5';
+		}
+		if (keyboard_helper(4+53*6, 239+8, p)){
+			printf("6");
+			return '6';
+		}
+		if (keyboard_helper(4+53*7, 239+8, p)){
+			printf("7");
+			return '7';
+		}
+		if (keyboard_helper(4+53*8, 239+8, p)){
+			printf("8");
+			return '8';
+		}
+		if (keyboard_helper(4+53*9, 239+8, p)){
+			printf("9");
+			return '9';
+		}
+		if (keyboard_helper(4+53*10, 239+8, p)){
+			printf("0");
+			return '0';
+		}
+		if (keyboard_helper(4+53*11, 239+8, p)){
+			printf("-");
+			return '-';
+		}
+		if (keyboard_helper(4+53*12, 239+8, p)){
+			printf("+");
+			return '+';
+		}
+		if (keyboard_helper(4+53*13, 239+8, p)){
+			printf("delete");
+			return '`';
+		}
+		if (keyboard_helper(4+53*14, 239+8, p)){
+			printf("delete");
+			return '`';
+		}
+
+		//2nd line
+		if (keyboard_helper(29+53*0, 239+8+40+6, p)){
+			printf("Q");
+			return 'Q';
+		}
+		if (keyboard_helper(29+53*1, 239+8+40+6, p)){
+			printf("W");
+			return 'W';
+		}
+		if (keyboard_helper(29+53*2, 239+8+40+6, p)){
+			printf("E");
+			return 'E';
+		}
+		if (keyboard_helper(29+53*3, 239+8+40+6, p)){
+			printf("R");
+			return 'R';
+		}
+		if (keyboard_helper(29+53*4, 239+8+40+6, p)){
+			printf("T");
+			return 'T';
+		}
+		if (keyboard_helper(29+53*5, 239+8+40+6, p)){
+			printf("Y");
+			return 'Y';
+		}
+		if (keyboard_helper(29+53*6, 239+8+40+6, p)){
+			printf("U");
+			return 'U';
+		}
+		if (keyboard_helper(29+53*7, 239+8+40+6, p)){
+			printf("I");
+			return 'I';
+		}
+		if (keyboard_helper(29+53*8, 239+8+40+6, p)){
+			printf("O");
+			return 'O';
+		}
+		if (keyboard_helper(29+53*9, 239+8+40+6, p)){
+			printf("P");
+			return 'P';
+		}
+		if (keyboard_helper(29+53*10, 239+8+40+6, p)){
+			printf("_");
+			return '_';
+		}
+		if (keyboard_helper(29+53*11, 239+8+40+6, p)){
+			printf("=");
+			return '=';
+		}
+		if (keyboard_helper(29+53*12, 239+8+40+6, p)){
+			printf("[");
+			return '[';
+		}
+		if (keyboard_helper(29+53*13, 239+8+40+6, p)){
+			printf("]");
+			return ']';
+		}
+
+		//3rd line
+		if (keyboard_helper(58+53*0, 239+8+40+6+40+6, p)){
+			printf("A");
+			return 'A';
+		}
+		if (keyboard_helper(58+53*1, 239+8+40+6+40+6, p)){
+			printf("S");
+			return 'S';
+		}
+		if (keyboard_helper(58+53*2, 239+8+40+6+40+6, p)){
+			printf("D");
+			return 'D';
+		}
+		if (keyboard_helper(58+53*3, 239+8+40+6+40+6, p)){
+			printf("F");
+			return 'F';
+		}
+		if (keyboard_helper(58+53*4, 239+8+40+6+40+6, p)){
+			printf("G");
+			return 'G';
+		}
+		if (keyboard_helper(58+53*5, 239+8+40+6+40+6, p)){
+			printf("H");
+			return 'H';
+		}
+		if (keyboard_helper(58+53*6, 239+8+40+6+40+6, p)){
+			printf("J");
+			return 'J';
+		}
+		if (keyboard_helper(58+53*7, 239+8+40+6+40+6, p)){
+			printf("K");
+			return 'K';
+		}
+		if (keyboard_helper(58+53*8, 239+8+40+6+40+6, p)){
+			printf("L");
+			return 'L';
+		}
+		if (keyboard_helper(58+53*9, 239+8+40+6+40+6, p)){
+			printf("{");
+			return '{';
+		}
+		if (keyboard_helper(58+53*10, 239+8+40+6+40+6, p)){
+			printf("}");
+			return '}';
+		}
+		if (keyboard_helper(58+53*11, 239+8+40+6+40+6, p)){
+			printf(":");
+			return ':';
+		}if (keyboard_helper(58+53*12, 239+8+40+6+40+6, p)){
+			printf(";");
+			return ';';
+		}
+		if (keyboard_helper(58+53*13, 239+8+40+6+40+6, p)){
+			printf("<");
+			return '<';
+		}
+		//4th line
+		if (keyboard_helper(87+53*0, 239+8+40+6+40+6+40+6, p)){
+			printf("Z");
+			return 'Z';
+		}
+		if (keyboard_helper(87+53*1, 239+8+40+6+40+6+40+6, p)){
+			printf("X");
+			return 'X';
+		}
+		if (keyboard_helper(87+53*2, 239+8+40+6+40+6+40+6, p)){
+			printf("C");
+			return 'C';
+		}
+		if (keyboard_helper(87+53*3, 239+8+40+6+40+6+40+6, p)){
+			printf("V");
+			return 'V';
+		}
+		if (keyboard_helper(87+53*4, 239+8+40+6+40+6+40+6, p)){
+			printf("B");
+			return 'B';
+		}
+		if (keyboard_helper(87+53*5, 239+8+40+6+40+6+40+6, p)){
+			printf("N");
+			return 'N';
+		}
+		if (keyboard_helper(87+53*6, 239+8+40+6+40+6+40+6, p)){
+			printf("M");
+			return 'M';
+		}
+		if (keyboard_helper(87+53*7, 239+8+40+6+40+6+40+6, p)){
+			printf(">");
+			return '>';
+		}
+		if (keyboard_helper(87+53*8, 239+8+40+6+40+6+40+6, p)){
+			printf(",");
+			return ',';
+		}		
+		if (keyboard_helper(87+53*9, 239+8+40+6+40+6+40+6, p)){
+			printf(".");
+			return '.';
+		}
+		if (keyboard_helper(87+53*10, 239+8+40+6+40+6+40+6, p)){
+			printf("?");
+			return '?';
+		}
+		if (keyboard_helper(87+53*11, 239+8+40+6+40+6+40+6, p)){
+			printf("/");
+			return '/';
+		}
+		if (keyboard_helper(87+53*12, 239+8+40+6+40+6+40+6, p)){
+			printf("!");
+			return '!';
+		}
+		//5th line
+		if (keyboard_helper(4+53*0, 239+8+40+6+40+6+40+6+40+6, p)){
+			printf("@");
+			return '@';
+		}
+		if (keyboard_helper(4+53*1, 239+8+40+6+40+6+40+6+40+6, p)){
+			printf("#");
+			return '#';
+		}
+		if (keyboard_helper(4+53*2, 239+8+40+6+40+6+40+6+40+6, p)){
+			printf("$");
+			return '$';
+		}
+		if (keyboard_helper(4+53*3, 239+8+40+6+40+6+40+6+40+6, p)){
+			printf("%%");
+			return '%';
+		}
+		//space 1
+		if (keyboard_helper(4+53*4, 239+8+40+6+40+6+40+6+40+6, p)){
+			printf("space");
+			return ' ';
+		}
+		//space 2
+		if (keyboard_helper(4+53*4+50*1, 239+8+40+6+40+6+40+6+40+6, p)){
+			printf("space");
+			return ' ';
+		}
+		//space 3
+		if (keyboard_helper(4+53*4+50*2, 239+8+40+6+40+6+40+6+40+6, p)){
+			printf("space");
+			return ' ';
+		}
+		//space 4
+		if (keyboard_helper(4+53*4+50*3, 239+8+40+6+40+6+40+6+40+6, p)){
+			printf("space");
+			return ' ';
+		}
+		//space 5
+		if (keyboard_helper(4+53*4+50*4, 239+8+40+6+40+6+40+6+40+6, p)){
+			printf("space");
+			return ' ';
+		}
+		//space 6
+		if (keyboard_helper(4+53*4+50*5, 239+8+40+6+40+6+40+6+40+6, p)){
+			printf("space");
+			return ' ';
+		}
+		if (keyboard_helper(4+53*5+50*5, 239+8+40+6+40+6+40+6+40+6, p)){
+			printf("^");
+			return '^';
+		}
+		if (keyboard_helper(4+53*6+50*5, 239+8+40+6+40+6+40+6+40+6, p)){
+			printf("&");
+			return '&';
+		}
+		if (keyboard_helper(4+53*7+50*5, 239+8+40+6+40+6+40+6+40+6, p)){
+			printf("*");
+			return '*';
+		}
+		if (keyboard_helper(4+53*8+50*5, 239+8+40+6+40+6+40+6+40+6, p)){
+			printf("(");
+			return '(';
+		}
+		if (keyboard_helper(4+53*9+50*5, 239+8+40+6+40+6+40+6+40+6, p)){
+			printf(")");
+			return ')';
+		}
+
+
 		if(p.x < 239){
 			break;
 		}
