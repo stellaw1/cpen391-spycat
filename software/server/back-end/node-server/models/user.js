b513a8488279c22fb367bd68fb64b36f35b27395
Rain Zhang <rainzhang2000@hotmail.com>
2022-03-25 02:50:18 -0700

    wip user routes

--- software/server/back-end/node-server/models/user.js
+++ software/server/back-end/node-server/models/user.js
@@ -1,1 +1,18 @@
 const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    id: {
+        type: Number,
+        required: true
+    },
+    pet_colour: {
+        type: String,
+        required: true
+    },
+    friendship_points: {
+        type: Number,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('user', userSchema)
