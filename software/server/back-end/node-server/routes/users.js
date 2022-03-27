5ff26097a63cbc1ec11a4ba34145e1893a6b570d
Rain Zhang <rainzhang2000@hotmail.com>
2022-03-27 15:33:12 -0700

    post route added

--- software/server/back-end/node-server/routes/users.js
+++ software/server/back-end/node-server/routes/users.js
@@ -1,9 +1,34 @@
 const express = require('express')
 const router = express.Router()
-
-module.exports = router
+const User = require('../models/user')
 
 // GET all
-router.get('/', (req, res) => {
-    res.send('test')
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find()
+        res.json(users)
+    } catch (err) {
+        res.status(500).json({message: err.message})
+    }
 })
+
+// GET one
+// router.get('/:id')
+
+
+// POST one
+router.post('/', async (req, res) => {
+    const user = new User({
+        id: req.body.id,
+        pet_colour: req.body.pet_colour,
+        friendship_points: 0
+    })
+    try {
+        const newUser = await user.save()
+        res.status(201).json(newUser)
+    } catch (err) {
+        res.status(400).json({message: err.message})
+    }
+})
+
+module.exports = router
