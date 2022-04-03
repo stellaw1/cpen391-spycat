fcb91e3780aad6ca5e3545c4647eb3b5af9ed538
arnold.ying <arnold.ying@salesforce.com>
2022-04-03 16:46:13 -0700

    update python3 call

--- software/motion-detection/python3_call.py
+++ software/motion-detection/python3_call.py
@@ -3,6 +3,10 @@ import subprocess
 script = ["python2.7", "motion_detection.py"]    
 process = subprocess.Popen(" ".join(script),
                                         shell=True,  
-                                        env={"PYTHONPATH": "."})
-print("finished calling py2 script")
-print(output)
+                                        stdout=subprocess.PIPE)
+
+output, error = process.communicate()  # receive output from the python2 script
+
+# script will not execute here unless python2 script exits
+
+# TODO: add ret
